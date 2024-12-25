306c4c5a405118377b81499146b84f4b061fc54b
harbissr <harbissr@gmail.com>
2024-12-24 23:29:18 -0500

    some css changes

--- Personal_Project/Rendezvous_1.2/Frontend/src/pages/HomePage.jsx
+++ Personal_Project/Rendezvous_1.2/Frontend/src/pages/HomePage.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
 import "./HomePage.css";
 import Weather from "../components/Weather";
 import DateCard from "../components/DateCard";
-import CreateEvent from "../components/CreateEvent";
-import EventList from "../components/EventList";
+
 
 export default function HomePage() {
 
@@ -14,10 +11,13 @@ export default function HomePage() {
         <div className="home-container">
             <div className="main-content">
                 <h2>Home</h2>
+                
+            </div>
+            <div className="date-card-container">
+                <DateCard />
             </div>
             <div className="sidebar">
                 <Weather />
-                <DateCard />
             </div>
         </div>
     </>
